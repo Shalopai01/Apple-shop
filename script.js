a332e5af625896a72d30997da1cc8110cb9c5d42
Shalopai01 <153362835+Shalopai01@users.noreply.github.com>
2024-09-07 19:32:43 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const images = [
-  "https://i.pinimg.com/736x/d8/d3/72/d8d3722f7d03da4fc826c2c13c0b56f6.jpg",
-];
-let imgindicator = 0;
-document.getElementById("btnf").addEventListener("click", function () {
-  document.getElementById("btnf").src =
-    "https://i.pinimg.com/736x/d8/d3/72/d8d3722f7d03da4fc826c2c13c0b56f6.jpg";
-});
